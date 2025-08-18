8199c9f585369fc28719028373635bbcc2509eac
techreviewnerdsinfo-code <techreviewnerdsinfo@gmail.com>
2025-08-18 10:21:28 -0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
