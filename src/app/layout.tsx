94024e2d3c65204c656653ea0f6ce5fd58f64a74
techreviewnerdsinfo-code <techreviewnerdsinfo@gmail.com>
2025-08-19 11:41:25 -0700

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import NavBar from '@/components/NavBar';
-import Footer from '@/components/Footer';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
